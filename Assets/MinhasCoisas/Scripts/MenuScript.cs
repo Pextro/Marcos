cb540e579337741c0f98400907f7cb1048292086
Pextro <70188351+Pextro@users.noreply.github.com>
2020-10-17 00:22:09 -0300

    Complemento
    Ultimos recursos para o projeto

---
+++ Assets/MinhasCoisas/Scripts/MenuScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    Button butao;
+    
+ void Start()
+    {
+        butao = GameObject.Find("Play").GetComponent<Button>();
+        butao.onClick.AddListener(Iniciodejogo);
+    }
+    // Update is called once per frame
+    public void Iniciodejogo()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
